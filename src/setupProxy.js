e62bd93d5045d1bed3fae451369c2dccefd34020
Ty Eggen <tycho.eggen@gmail.com>
2020-07-23 02:49:21 -0700

    Improved development proxy configuration

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,6 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/auth',
-    createProxyMiddleware({
-      target: 'http://localhost:7000',
-      changeOrigin: true,
-    })
-  );
+module.exports = function (app) {
+  app.use('/api', createProxyMiddleware({ target: 'http://localhost:7000', changeOrigin: true, }));
+  app.use('/auth', createProxyMiddleware({ target: 'http://localhost:7000', changeOrigin: true, }));
 };
